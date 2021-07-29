d79987b87f0203669144253c7b50e79327c2b6ae
picu63 <picu63@gmail.com>
2021-07-29 16:49:20 +0200

    Add project files.

---
+++ StartMenuBackup.Console/Program.cs
@@ -0,0 +1,12 @@
+﻿
+namespace StartMenuBackup.Console
+{
+    using System;
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
